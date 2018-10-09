5503a599ab89355e5960dba75a9fccb954b50234
lizhao <lizhao.mail@qq.com>
2018-10-09 11:18:03 +0800

    network check

--- app/src/main/java/com/example/lizhao/weather/MainActivity.java
+++ app/src/main/java/com/example/lizhao/weather/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.lizhao.weather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.lizhao.util.NetUtil;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE){
+            Log.d("myWeather","网络OK!");
+            Toast.makeText(MainActivity.this,"网络OK!",Toast.LENGTH_LONG).show();
+        }else{
+            Log.d("myWeather","网络挂了");
+            Toast.makeText(MainActivity.this,"网络挂了!",Toast.LENGTH_LONG).show();
+        }
     }
 }
